406cf7f47198bf5c6d4c53f432821b2c72b11912
Sebastian Alfonso <jsalfonso00@misena.edu.co>
2021-07-24 20:52:22 -0500

    Insertando libreria Scanner y objeto entrada

--- ProbandoGit/src/com/company/Main.java
+++ ProbandoGit/src/com/company/Main.java
@@ -1,10 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner entrada = new Scanner(System.in);
+
         System.out.println("Hello World!");
-	    nextLine();
+	    entrada.next();
     }
+
 }
+
